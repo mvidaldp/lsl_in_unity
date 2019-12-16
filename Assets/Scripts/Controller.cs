d335c2e5ce4956089ba61722ca25fbf6a7bb1af0
mvidaldp <mvidal.dp@gmail.com>
2019-12-16 16:57:00 +0100

    Fix distorted audio (set right compression format), errors and style

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,5 +1,4 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
@@ -8,15 +7,14 @@ public class Controller : MonoBehaviour
         - Clean and comment code. Generate build and try it on windows
         - Try Unity4LSL on it
      */
+    // colors to show
     public Color colorOne = Color.black;
     public Color colorTwo = Color.white;
 
     private Camera _cam; // where we actually change the colors
 
-    public AudioSource audioSource;
-
     // FPS related vars:
-    // not enough with just enum declaration to be in the inspector, it needs an instantiation
+    // it needs further instantiation as it's just an enumerator declaration
     public enum VSync
     {
         Max = 1, // 1 frame per VSync
@@ -28,6 +26,9 @@ public class Controller : MonoBehaviour
     public VSync vSync = VSync.Max; // enum instantiation
     public bool vSyncEnabled;
     public int targetFrameRate = 60;
+
+    // audio related vars
+    private AudioSource _audioSource; // to play an audio clip
 
     public enum Key
     {
@@ -45,20 +46,20 @@ public class Controller : MonoBehaviour
         B
     }
 
-    public Key key = Key.A;
-    public int[] octaves = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    public int octave = 4;
+    public Key key = Key.A; // set A as default key
+    public int octave = 4; // default octave
     public int repeatToneAfter = 500; // tone repetition delay in ms
 
     private float _fps;
     private float _referenceTime;
     private float _durationReference;
+    private float _audioReference;
 
     // time in ms
     public int refreshTime = 1000;
     public int durationOne = 200;
     public int durationTwo = 50;
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -72,7 +73,8 @@ public class Controller : MonoBehaviour
         _cam.clearFlags = CameraClearFlags.SolidColor;
         _referenceTime = Time.realtimeSinceStartup * 1000;
         _durationReference = Time.realtimeSinceStartup * 1000;
-        audioSource = GetComponent<AudioSource>();
+        _audioReference = Time.realtimeSinceStartup * 1000;
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -82,36 +84,34 @@ public class Controller : MonoBehaviour
         var timeSinceStart = Time.realtimeSinceStartup * 1000;
         var elapsedTime = timeSinceStart - _referenceTime;
         var showTime = timeSinceStart - _durationReference;
-        var showSum = durationOne + durationTwo;
+        var lastPlay = timeSinceStart - _audioReference;
 
-        if (showTime >= showSum)
+        if (showTime >= durationOne + durationTwo)
         {
             _cam.backgroundColor = colorOne;
             _durationReference = timeSinceStart;
         }
-        else if (showTime > durationOne && showTime < showSum)
+        else if (showTime > durationOne && showTime < durationOne + durationTwo)
         {
             _cam.backgroundColor = colorTwo;
         }
 
-        if (elapsedTime >= repeatToneAfter)
+        if (lastPlay >= repeatToneAfter)
         {
+            _audioReference = timeSinceStart;
             var tone = Resources.Load<AudioClip>("Media/Audio/" + key + octave);
-            audioSource.PlayOneShot(tone);
+            _audioSource.PlayOneShot(tone);
         }
 
-        if (elapsedTime >= refreshTime)
-        {
-            Debug.Log("FPS: " + (1000 * _fps) / elapsedTime);
-            _fps = 0;
-            _referenceTime = timeSinceStart;
-
-            // update FPS settings
-            if (!vSyncEnabled)
-                QualitySettings.vSyncCount = 0;
-            else
-                QualitySettings.vSyncCount = (int) vSync;
-            Application.targetFrameRate = targetFrameRate;
-        }
+        if (!(elapsedTime >= refreshTime)) return; // what follows is like putting it into an else block
+        Debug.Log("FPS: " + (1000 * _fps) / elapsedTime);
+        _fps = 0;
+        _referenceTime = timeSinceStart;
+        // update FPS settings
+        if (!vSyncEnabled)
+            QualitySettings.vSyncCount = 0;
+        else
+            QualitySettings.vSyncCount = (int) vSync;
+        Application.targetFrameRate = targetFrameRate;
     }
 }
