f6fc939e0930106606a9ee693172aab155bfddd1
mvidaldp <mvidal.dp@gmail.com>
2020-06-09 00:58:14 +0200

    Simplify and unify scripts, remove editor code, add comments

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,126 +1,120 @@
 ﻿using System;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
 
 public class Controller : MonoBehaviour
 {
-    // colors to show
-    public Color32 colorOne = Color.black;
-    public Color32 colorTwo = Color.white;
-    public bool isPlaying = false;
-    public bool colorChanged = false;
-
-    private Camera _cam; // where we actually change the colors
-
-    // FPS related vars:
-    // it needs further instantiation as it's just an enumerator declaration
-    public enum VSync
-    {
-        Max = 1, // 1 frame per VSync
-        Half = 2, // 1 frame each 2 VSync
-        Third = 3, // 1 out of 3
-        Fourth = 4 // 1 out of 4
-    }
-
-    public VSync vSync = VSync.Max; // enum instantiation
-    public bool vSyncEnabled;
-    public int targetFrameRate = 90;
-
-    // audio related vars
+    // Game components
+    private Camera _cam; // where the background color is changed
     private AudioSource _audioSource; // to play an audio clip
 
-    public enum Key
-    {
-        C,
-        Db,
-        D,
-        Eb,
-        E,
-        F,
-        Gb,
-        G,
-        Ab,
-        A,
-        Bb,
-        B
-    }
+    // Colors to use as background (structs cannot be declared as const, only literals)
+    private static readonly Color32 ColorOne = Color.black; // first (default)
+    private static readonly Color32 ColorTwo = Color.white; // second (change, to detect)
 
-    public Key key = Key.A; // set A as default key
-    public int octave = 4; // default octave
-    public int repeatToneAfter = 500; // tone repetition delay in ms
+    private const bool Timestamp = true; // true for sending Unity internal timestamp since start
 
-    private float _fps;
-    private float _referenceTime;
-    private float _durationReference;
-    private float _audioReference;
+    private float _referenceTime; // time to check for audio and color change
+    private bool _isPlaying; // audio is playing
+    private bool _colorChanged; // color has changed from default
 
-    // time in ms
-    public int refreshTime = 1000;
-    public int durationOne = 950;
-    public int durationTwo = 50;
+    // LSL VARs
+    // streams info
+    private liblsl.StreamInfo _lslStreamInfoAudio;
+    private liblsl.StreamInfo _lslStreamInfoColor;
 
+    // streams outlets
+    private liblsl.StreamOutlet _lslOutletAudio;
+    private liblsl.StreamOutlet _lslOutletColor;
+
+    private const string LslStreamNameAudio = "Audio"; // audio stream name
+    private const string LslStreamNameColor = "Diode"; // color stream name
+    private const string LslStreamType = "Marker"; // stream type (not relevant, just a label)
+    private int _lslChannelCount; // length of the data to be sent (depends on Timestamp)
+    private const double NominalRate = liblsl.IRREGULAR_RATE; // assuming irregular frame rate
+
+    // ch format as float since we send {currentFrame (int), changed (0/1 : int), timestamp (seconds with decimals)}
+    private const liblsl.channel_format_t LslChannelFormat = liblsl.channel_format_t.cf_float32;
+
+    // global unique identifiers
+    private string _guidColor;
+    private string _guidAudio;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!vSyncEnabled)
-            QualitySettings.vSyncCount = 0;
-        else
-            QualitySettings.vSyncCount = (int) vSync;
-        Application.targetFrameRate = targetFrameRate;
-        _cam = GetComponent<Camera>();
-        _cam.clearFlags = CameraClearFlags.SolidColor;
-        _referenceTime = Time.realtimeSinceStartup * 1000;
-        _durationReference = Time.realtimeSinceStartup * 1000;
-        _audioReference = Time.realtimeSinceStartup * 1000;
-        _audioSource = GetComponent<AudioSource>();
+        // VARs initialization
+        // get components and set background color
+        _audioSource = GetComponent<AudioSource>(); // audio source to handle audio playing
+        _cam = GetComponent<Camera>(); // camera to handle background color
+        _cam.backgroundColor = ColorOne; // background color, set first (default)
+
+        _isPlaying = false; // not playing
+        _colorChanged = false; // not changed (first color)
+        _referenceTime = 0.5f; // time until change in seconds (play audio, change color)
+
+        _lslChannelCount = Timestamp ? 3 : 2; // length of the data to be sent (+1 if Timestamp)
+
+        _guidAudio = Guid.NewGuid().ToString(); // generate GUID for audio stream
+        _guidColor = Guid.NewGuid().ToString(); // generate GUID for color stream
+
+        // audio stream definition
+        _lslStreamInfoAudio = new liblsl.StreamInfo(
+            LslStreamNameAudio,
+            LslStreamType,
+            _lslChannelCount,
+            NominalRate,
+            LslChannelFormat,
+            _guidAudio);
+
+        // color stream definition
+        _lslStreamInfoColor = new liblsl.StreamInfo(
+            LslStreamNameColor,
+            LslStreamType,
+            _lslChannelCount,
+            NominalRate,
+            LslChannelFormat,
+            _guidColor);
+
+        // create the outlet streams from the stream info defined before
+        _lslOutletColor = new liblsl.StreamOutlet(_lslStreamInfoColor);
+        _lslOutletAudio = new liblsl.StreamOutlet(_lslStreamInfoAudio);
     }
 
-    // Update is called once per frame
-    void FixedUpdate() // check the difference with FixedUpdate(), meant for higher sampling frequency
+    // FixedUpdate has the frequency of the physics system; it is called every fixed frame-rate frame
+    void FixedUpdate()
     {
-        ++_fps;
-        var timeSinceStart = Time.realtimeSinceStartup * 1000;
-        var elapsedTime = timeSinceStart - _referenceTime;
-        var showTime = timeSinceStart - _durationReference;
-        var lastPlay = timeSinceStart - _audioReference;
-        
-//        if (showTime >= durationOne + durationTwo)
-//        {
-//            _cam.backgroundColor = colorOne;
-//            _durationReference = timeSinceStart;
-//        }
-//        else if (showTime > durationOne && showTime < durationOne + durationTwo)
-//        {
-//            _cam.backgroundColor = colorTwo;
-//        }
-
-        if (lastPlay >= repeatToneAfter)
+        _referenceTime -= Time.deltaTime; // subtract the completion time in seconds since the last frame
+        if (_referenceTime <= 0) // when ref time reaches 0
         {
-            _audioReference = timeSinceStart;
-            //var tone = Resources.Load<AudioClip>("Media/Audio/" + key + octave);
-            //_audioSource.PlayOneShot(tone);
-            _audioSource.Play();
-            _cam.backgroundColor = colorTwo;
+            _audioSource.Play(); // play audio clip (if any specified)
+            _cam.backgroundColor = ColorTwo; // set background color to second (change, to detect)
+            _referenceTime = 0.5f; // reset ref time
         }
-        else
-            _cam.backgroundColor = colorOne;
-        
-        isPlaying = _audioSource.isPlaying;
-        colorChanged = _cam.backgroundColor == colorTwo;
+        else _cam.backgroundColor = ColorOne; // background color back to first (default)
 
-        if (!(elapsedTime >= refreshTime)) return; // what follows is like putting it into an else block
-        Debug.Log("FPS: " + (1000 * _fps) / elapsedTime);
-        
-        _fps = 0;
-        _referenceTime = timeSinceStart;
-        
-        // update FPS settings
-        if (!vSyncEnabled)
-            QualitySettings.vSyncCount = 0;
-        else
-            QualitySettings.vSyncCount = (int) vSync;
-        Application.targetFrameRate = targetFrameRate;
+        _colorChanged = _cam.backgroundColor == ColorTwo; // if color has been changed (from default/first)
+        _isPlaying = _audioSource.isPlaying; // if audio is playing
+
+        var cFrame = Time.frameCount; // current frame since start
+        var time = Time.realtimeSinceStartup; // timestamp since start
+
+        // define data arrays to be sent (push) 
+        var dataColor = new float[_lslChannelCount];
+        var dataAudio = new float[_lslChannelCount];
+
+        // fill arrays (current frame, changed/playing, timestamp)
+        dataColor[0] = cFrame;
+        dataAudio[0] = cFrame;
+        dataColor[1] = _colorChanged ? 1 : 0; // changed/playing encoded as 1/0 since LSL has no bool type
+        dataAudio[1] = _isPlaying ? 1 : 0;
+        if (_lslChannelCount == 3) // if Timestamp
+        {
+            dataColor[2] = time;
+            dataAudio[2] = time;
+        }
+
+        // send data
+        _lslOutletColor.push_sample(dataColor);
+        _lslOutletAudio.push_sample(dataAudio);
     }
 }
